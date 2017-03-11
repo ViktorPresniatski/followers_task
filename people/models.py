f0c8796bcdafcf4595342b70e7ccf334e3cc8e88
ViktorPresniatski <presniatski@gmail.com>
2017-03-12 01:50:22 +0300

    change schema person

--- people/models.py
+++ people/models.py
@@ -8,21 +8,32 @@ class Person(models.Model):
     id = models.IntegerField(primary_key=True)  # AutoField?
     name = models.CharField(max_length=32)
     follow_ids = models.TextField()
+    following = models.ManyToManyField(
+        'self',
+        symmetrical=False,
+        related_name='followers',
+    )
 
     class Meta:
         db_table = 'people'
 
-    def followings(self):
-        array_of_followings = []
-        for id in self.follow_ids.split():
-            array_of_followings.extend(int(id))
-        return array_of_followings
+    def follow(self, *people):
+        self.following.add(*people)
+
+    def unfollow(self, *people):
+        self.following.remove(*people)
+
+    # def followings(self):
+    #     array_of_followings = []
+    #     for id in self.follow_ids.split():
+    #         array_of_followings.extend(int(id))
+    #     return array_of_followings
 
     def count_following(self):
-        return len(self.follow_ids.split())
+        return self.following.count()
 
     def count_followers(self):
-        count = 0
-        for p in Person.objects.all():
-            count += p.follow_ids.split().count(str(self.id))
-        return count
+        return self.followers.count()
+
+    def __str__(self):
+        return "id: {0}, name: {1}".format(self.id, self.name)
